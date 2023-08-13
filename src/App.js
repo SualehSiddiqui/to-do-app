98891315ce75cc24f561e7822d2c61524cb2c959
SualehSiddiqui <sualehmsiddiqui@gmail.com>
2023-08-14 00:05:43 +0500

    working

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Header from './Components/Header/index';
+
+function App() {
+  return(
+    // <div className='background-div'>
+      <Header />
+
+    // </div>
+  )
+}
+
+
+// console. disableYellowBox = true;
+export default App;
